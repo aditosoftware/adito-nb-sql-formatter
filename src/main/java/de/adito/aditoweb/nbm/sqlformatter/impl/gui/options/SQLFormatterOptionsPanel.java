c63b454a42c642a6505a7d01a7065812417a5b13
p.neub <p.neub@adito.de>
2021-02-04 11:14:24 +0100

    feat: GUI group description fix: improved example sql

--- src/main/java/de/adito/aditoweb/nbm/sqlformatter/impl/gui/options/SQLFormatterOptionsPanel.java
+++ src/main/java/de/adito/aditoweb/nbm/sqlformatter/impl/gui/options/SQLFormatterOptionsPanel.java
@@ -18,7 +18,7 @@ import java.util.Objects;
  */
 public class SQLFormatterOptionsPanel extends JPanel
 {
-  private final JTextArea inputSql = new JTextArea("select  case   when CONTACT.PERSON_ID is null then 1   else case    when trim (CONTACT.ORGANISATION_ID  ) = '0' then 2    else 3   end  end,     ADDRESS.ADDRESS,   ADDRESS.BUILDINGNO,  ADDRESS.ZIP, ADDRESS.CITY,     ADDRESS.COUNTRY,  ADDRESS.ADDRESSADDITION, ADDRESS.ADDRIDENTIFIER, ADDRESS.DISTRICT,  ADDRESS.REGION, ADDRESS.PROVINCE,     CONTACT.DEPARTMENT,     CONTACT.CONTACTROLE,     CONTACT.CONTACTPOSITION,     CONTACT.LETTERSALUTATION,     ORGANISATION.name,     PERSON.FIRSTNAME,     PERSON.MIDDLENAME,     PERSON.LASTNAME,     PERSON.SALUTATION,     PERSON.TITLE,     PERSON.TITLESUFFIX,  coalesce ( CONTACT.ISOLANGUAGE,  (  select   c.ISOLANGUAGE  from CONTACT  c   where  c.ORGANISATION_ID = CONTACT.ORGANISATION_ID                 and PERSON_ID is null         )     ),     '',     '',     '',     (   select ADDR_FORMAT from AB_COUNTRYINFO  where  ISO2 = ADDRESS.COUNTRY     ), ADDRESS.ADDR_TYPE from CONTACT join PERSON     on CONTACT.PERSON_ID = PERSON.PERSONID left join ORGANISATION     on CONTACT.ORGANISATION_ID = ORGANISATION.ORGANISATIONID left join ADDRESS     on CONTACT.ADDRESS_ID = ADDRESS.ADDRESSID where     CONTACT.CONTACTID in (  '0ae3a779-131c-440b-a653-569ac25ea699' )  and TEST = 123 and TEST = 123  or SDF = 45");
+  private final JTextArea inputSql = new JTextArea("select  case   when CONTACT.PERSON_ID is null then 1   else case    when trim (CONTACT.ORGANISATION_ID  ) = '0' then 2    else 3   end  end,     ADDRESS.ADDRESS,   ADDRESS.BUILDINGNO,  ADDRESS.ZIP, ADDRESS.CITY,     ADDRESS.COUNTRY,  ADDRESS.ADDRESSADDITION, ADDRESS.ADDRIDENTIFIER, ADDRESS.DISTRICT,  ADDRESS.REGION, ADDRESS.PROVINCE,     CONTACT.DEPARTMENT,     CONTACT.CONTACTROLE,     CONTACT.CONTACTPOSITION,     CONTACT.LETTERSALUTATION,     ORGANISATION.name,     PERSON.FIRSTNAME,     PERSON.MIDDLENAME,     PERSON.LASTNAME,     PERSON.SALUTATION,     PERSON.TITLE,     PERSON.TITLESUFFIX,  coalesce ( CONTACT.ISOLANGUAGE,  (  select   c.ISOLANGUAGE  from CONTACT  c   where  c.ORGANISATION_ID = CONTACT.ORGANISATION_ID                 and PERSON_ID is null         )     ),      'Max Musterman',     (   select ADDR_FORMAT from AB_COUNTRYINFO  where  ISO2 = ADDRESS.COUNTRY     ), ADDRESS.ADDR_TYPE from CONTACT join PERSON     on CONTACT.PERSON_ID = PERSON.PERSONID left join ORGANISATION     on CONTACT.ORGANISATION_ID = ORGANISATION.ORGANISATIONID left join ADDRESS     on CONTACT.ADDRESS_ID = ADDRESS.ADDRESSID where     CONTACT.CONTACTID in (  '0ae3a779-131c-440b-a653-569ac25ea699' )  and TEST = 123 and TEST = 123  or SDF = 45");
   private final JTextArea outputSql = new JTextArea();
 
   private final JComboBox<ELetterCaseMode> wordCaseMode = new JComboBox<>(ELetterCaseMode.values());
@@ -37,12 +37,16 @@ public class SQLFormatterOptionsPanel extends JPanel
 
     JPanel leftPanel = new JPanel();
     leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-    addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_WORD_CASE"), wordCaseMode);
-    addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_KEYWORD_CASE"), keywordCaseMode);
-    addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_NEWLINE_COMMA"), newlineBeforeComma);
-    addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_PLUS_RIGHT"), copyToStringPlusRight);
-    addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_GAP_INSIDE_QUOTES"), gapInsideQuotes);
     add(leftPanel);
+
+    _addGroupLabel(leftPanel, "Formatting");
+    _addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_WORD_CASE"), wordCaseMode);
+    _addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_KEYWORD_CASE"), keywordCaseMode);
+    _addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_NEWLINE_COMMA"), newlineBeforeComma);
+
+    _addGroupLabel(leftPanel, "CopyToString");
+    _addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_PLUS_RIGHT"), copyToStringPlusRight);
+    _addConfigComponent(leftPanel, NbBundle.getMessage(SQLFormatterOptionsPanel.class, "LBL_OptionsPanel_CONF_GAP_INSIDE_QUOTES"), gapInsideQuotes);
 
     wordCaseMode.addActionListener(e -> _update());
     keywordCaseMode.addActionListener(e -> _update());
@@ -83,15 +87,42 @@ public class SQLFormatterOptionsPanel extends JPanel
     });
   }
 
-  private void addConfigComponent(@NotNull JPanel panel, @NotNull String text, @NotNull Component component)
+  /**
+   * Adds a group label to the given JPanel
+   *
+   * @param pPanel the target panel
+   * @param pText the label text
+   */
+  private void _addGroupLabel(@NotNull JPanel pPanel, @NotNull String pText)
+  {
+    pPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+    JPanel componentPanel = new JPanel();
+    componentPanel.setLayout(new GridLayout(1, 1));
+    componentPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 25));
+    JLabel label = new JLabel(pText);
+    label.setFont(new Font(label.getFont().getName(), Font.PLAIN, 16));
+    componentPanel.add(label);
+    componentPanel.add(new JLabel());
+    pPanel.add(componentPanel);
+    pPanel.add(Box.createRigidArea(new Dimension(0, 3)));
+  }
+
+  /**
+   * Adds a settings component the the given JPanel
+   *
+   * @param pPanel the target panel
+   * @param pText the component description
+   * @param pComponent the component witch will be added to the panel
+   */
+  private void _addConfigComponent(@NotNull JPanel pPanel, @NotNull String pText, @NotNull Component pComponent)
   {
     JPanel componentPanel = new JPanel();
     componentPanel.setLayout(new GridLayout(1, 1));
     componentPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 25));
-    componentPanel.add(new JLabel(text));
-    componentPanel.add(component);
-    panel.add(componentPanel);
-    panel.add(Box.createRigidArea(new Dimension(0, 3)));
+    componentPanel.add(new JLabel(pText));
+    componentPanel.add(pComponent);
+    pPanel.add(componentPanel);
+    pPanel.add(Box.createRigidArea(new Dimension(0, 3)));
   }
 
   /**
